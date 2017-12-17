61574ad29454e5f81f9437b6e30c5bb9296683ba
Stian Lund Johansen <stian.lund.johansen@nrk.no>
2017-12-17 01:43:57 +0100

    I3: Make `merge_config.sh` work with more computers. Add some variables and bindings

--- config/i3/merge_config.sh
+++ config/i3/merge_config.sh
@@ -1,3 +1,18 @@
 #!/usr/bin/env bash
 
-cat $HOME/.config/i3/config.d/* > $HOME/.config/i3/config
+COMPUTER=$1
+CONFIG_FILE=$HOME/.config/i3/config
+
+rm $CONFIG_FILE
+
+for f in $HOME/.config/i3/config.d/*; do
+  filename=$(basename "$f")
+  extension="${filename##*.}"
+  section=$(sed '/# vim:filetype=i3/d' $f)
+  if [ $extension != 'work-laptop' ] && [ $extension != 'main-desktop' ]; then
+    echo -e "\n# FROM: $filename\n$section" >> $CONFIG_FILE
+  fi;
+  if [ $extension = $COMPUTER ]; then
+    echo -e "\n# FROM: $filename\n$section" >> $CONFIG_FILE
+  fi;
+done;
