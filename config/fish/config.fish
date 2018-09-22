476645c0e18dd415f51cdad49476f8211f21d123
Stian Lund Johansen <stian@lundjohansen.no>
2018-09-22 11:20:34 +0200

    Stuff

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,5 +1,9 @@
 # fish_vi_key_bindings
 set fish_greeting ""
 
+set -gx TERM xterm-256color
+set -gx LIBVIRT_DEFAULT_URI qemu:///system
+set -gx EDITOR nvim
+
 bind yy fish_clipboard_copy
 bind p fish_clipboard_paste
