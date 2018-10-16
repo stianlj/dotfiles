c473936ffb7c720f019ff96af7c7ef239b4981ca
Stian Lund Johansen <stian@lundjohansen.no>
2018-10-16 23:53:58 +0200

    Remove borders from qutebrowser. Fix some qutebrowser configs

--- config/qutebrowser/config.py
+++ config/qutebrowser/config.py
@@ -3,11 +3,8 @@ defaultFont = "20px Hack Nerd Font"
 defaultPadding = {"top": 10, "bottom": 10, "left": 10, "right": 10}
 
 # Config
-c.url.start_pages = ["https://google.com"]
-c.url.searchengines = {
-    'DEFAULT': 'https://www.google.com/search?q={}',
-    'd': 'https://duckduckgo.com/?q={}'
-}
+c.url.start_pages = ["https://duckduckgo.com"]
+c.url.searchengines = {'DEFAULT': 'https://duckduckgo.com/?q={}'}
 
 c.zoom.default = '150%'
 
@@ -21,7 +18,8 @@ c.fonts.completion.category = defaultFont
 c.fonts.completion.entry = defaultFont
 
 # Bindings
-config.bind('<Ctrl-Shift-y>', 'hint links spawn --detach mpv --force-window yes {hint-url}')
+config.bind(';yy', 'hint links spawn --detach mpv --force-window yes {hint-url}')
+config.bind(';ss', 'spawn --userscript search-site')
 
 # Themeing
 config.source('nord-qutebrowser.py')
