3d807b8458972ebba1582f025fbced599f926e6e
Stian Lund Johansen <stian@lundjohansen.no>
2018-10-18 23:04:28 +0200

    Autostart megasync

--- config/qutebrowser/config.py
+++ config/qutebrowser/config.py
@@ -1,12 +1,12 @@
 # Variables
-defaultFont = "20px Hack Nerd Font"
+defaultFont = "15px Hack Nerd Font"
 defaultPadding = {"top": 10, "bottom": 10, "left": 10, "right": 10}
 
 # Config
-c.url.start_pages = ["https://duckduckgo.com"]
+c.url.start_pages = ["https://start.duckduckgo.com"]
 c.url.searchengines = {'DEFAULT': 'https://duckduckgo.com/?q={}'}
 
-c.zoom.default = '150%'
+c.zoom.default = '125%'
 
 c.fonts.tabs = defaultFont
 c.tabs.padding = defaultPadding
@@ -18,7 +18,7 @@ c.fonts.completion.category = defaultFont
 c.fonts.completion.entry = defaultFont
 
 # Bindings
-config.bind(';yy', 'hint links spawn --detach mpv --force-window yes {hint-url}')
+config.bind(';my', 'hint links spawn --detach mpv --force-window yes {hint-url}')
 config.bind(';ss', 'spawn --userscript search-site')
 
 # Themeing
