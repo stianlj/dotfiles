cf479a184234b236b935539e7728804142addf54
Stian Lund Johansen <stian@lundjohansen.no>
2020-03-04 23:37:17 +0100

    Remove packages that are already in doom emacs

--- doom.d/packages.el
+++ doom.d/packages.el
@@ -1,8 +1,6 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; $DOOMDIR/packages.el
 
-(package! js2-mode)
-(package! tide)
 (package! prettier-js)
 (package! add-node-modules-path)
 (package! dockerfile-mode)
