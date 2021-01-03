0bed10e80316ac97a2f921890616c7daf60f0d00
Stian Lund Johansen <stian@lundjohansen.no>
2021-01-03 17:15:21 +0100

    Lot of tweaks
    + Unbind `D` in `qutebrowser`
    + Set variable pitch font and a unicode font
    + Make org-mode somewhat prettier, and more readable
    + Add deft, for easy org documents search
    + Remove `notmuch`
    + Add `parinfer` and `lispy` for better lisp handling
    + Set spellchecker to spell-fu
    + Set `+lsp` flag to shell scripting
    + Add `ranger` view to `dired`

--- doom.d/config.el
+++ doom.d/config.el
@@ -24,17 +24,23 @@
 
 ;; DOOM CONFIG
 (setq doom-theme 'doom-dracula)
-(setq doom-font (font-spec :family "JetBrainsMonoMedium Nerd Font Mono" :size 18)
-      doom-big-font (font-spec :family "JetBrainsMonoMedium Nerd Font Mono" :size 28))
+(setq doom-font (font-spec :family "JetBrainsMono Nerd Font Mono" :size 18)
+      doom-variable-pitch-font (font-spec :family "Roboto" :size 19)
+      doom-unicode-font (font-spec :family "Noto Color Emoji" :size 18)
+      doom-big-font (font-spec :family "JetBrainsMono Nerd Font Mono" :size 28))
 (after! doom-themes
   (setq doom-themes-enable-bold t
         doom-themes-enable-italic t))
+(setq mixed-pitch-set-height t)
+(set-face-attribute 'variable-pitch nil :height 180)
 (custom-set-faces!
   '(font-lock-comment-face :slant italic)
   '(font-lock-keyword-face :slant italic))
+
 (map! :leader
       :desc "Load new theme"
       "h t" #'counsel-load-theme)
+
 (setq fancy-splash-image
       (concat doom-private-dir "splash/doom-emacs-color2.svg"))
 
@@ -45,18 +51,20 @@
 
 ;; EDITOR
 (setq display-line-numbers-type 'relative)
+(after! git-gutter-fringe
+  (fringe-mode '10))
 
-(defun slj/set-fill-column ()
-  (setq display-fill-column-indicator-character ?│)
-  ;; (setq display-fill-column-indicator-character "│")
-  (setq fill-column 120))
+;; (defun slj/set-fill-column ()
+;;   (setq display-fill-column-indicator-character ?│)
+;;   ;; (setq display-fill-column-indicator-character "│")
+;;   (setq fill-column 120))
 
-(if (daemonp)
-    (add-hook 'after-make-frame-functions
-              (lambda (frame)
-                (with-selected-frame frame
-                  (slj/set-fill-column))))
-  (slj/set-fill-column))
+;; (if (daemonp)
+;;     (add-hook 'after-make-frame-functions
+;;               (lambda (frame)
+;;                 (with-selected-frame frame
+;;                   (slj/set-fill-column))))
+;;   (slj/set-fill-column))
 
 (setq evil-split-window-below t
       evil-vsplit-window-right t)
@@ -93,11 +101,7 @@
     (mu4e~headers-defun-mark-for spam)
     (define-key mu4e-headers-mode-map (kbd "S") 'mu4e-headers-mark-for-spam)
     )
-  (after! notmuch
-    (setq +notmuch-sync-backend 'offlineimap)
-  ))
-
-;; MAIL (notmuch)
+  )
 
 ;; PROJECTILE
 (when (string-match-p computer-identifier "work-laptop")
@@ -129,6 +133,39 @@
 ;; (if (eq 'computer-identifier "main-desktop")
 ;;       (setq org-agenda-files '("~/Nextcloud/Agenda/")))
 (setq org-roam-directory "~/Nextcloud/Documents/Org/Roam")
+
+(setq deft-directory "~/Nextcloud/Documents/Org"
+      deft-extensions '("org")
+      deft-recursive t)
+
+(defun slj/org-mode-visual()
+  (setq visual-fill-column-width 120
+        display-fill-column-indicator nil)
+  (visual-fill-column-mode 1))
+
+
+(add-hook! 'org-mode-hook #'mixed-pitch-mode #'doom-disable-line-numbers-h #'slj/org-mode-visual)
+
+(after! org
+  (custom-set-faces!
+    '(org-document-title :height 1.3)
+    '(org-level-1 :inherit outline-1 :weight extra-bold :height 1.4)
+    '(org-level-2 :inherit outline-2 :weight bold :height 1.15)
+    '(org-level-3 :inherit outline-3 :weight bold :height 1.12)
+    '(org-level-4 :inherit outline-4 :weight bold :height 1.09)
+    '(org-level-5 :inherit outline-5 :weight semi-bold :height 1.06)
+    '(org-level-6 :inherit outline-6 :weight semi-bold :height 1.03)
+    '(org-level-7 :inherit outline-7 :weight semi-bold)
+    '(org-level-8 :inherit outline-8 :weight semi-bold)
+    ;; Ensure that anything that should be fixed-pitch in org buffers appears that
+    ;; way
+    '(org-block nil :foreground nil :inherit 'fixed-pitch)
+    '(org-code nil   :inherit '(shadow fixed-pitch))
+    '(org-table nil   :inherit '(shadow fixed-pitch))
+    '(org-verbatim nil :inherit '(shadow fixed-pitch))
+    '(org-special-keyword nil :inherit '(font-lock-comment-face fixed-pitch))
+    '(org-meta-line nil :inherit '(font-lock-comment-face fixed-pitch))
+    '(org-checkbox nil :inherit 'fixed-pitch)))
 
 ;; JAVASCRIPT/TYPESCRIPT
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
