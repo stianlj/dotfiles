c26d38aa8c0ca31b745910d91d5817b53efd6878
Stian Lund Johansen <stian.lund.johansen@nrk.no>
2021-10-20 18:09:22 +0200

    No border on wezterm. Use FiraCode

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -1,8 +1,9 @@
 local wezterm = require 'wezterm';
 return {
-  font = wezterm.font("JetBrainsMonoMedium Nerd Font Mono"),
+  font = wezterm.font("FiraCode Nerd Font"),
   default_prog = {"/usr/bin/fish", "-l"},
   font_size = 13.0,
   harfbuzz_features = {"zero"},
   window_background_opacity = 0.9,
+  hide_tab_bar_if_only_one_tab = true,
 }
