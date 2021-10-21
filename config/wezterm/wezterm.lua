bd9881f478158966a3b81943c2756740f4f8f12b
Stian Lund Johansen <stian@lundjohansen.no>
2021-10-21 23:36:26 +0200

    chore: Set font size in wexterm

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -2,8 +2,12 @@ local wezterm = require 'wezterm';
 return {
   font = wezterm.font("FiraCode Nerd Font"),
   default_prog = {"/usr/bin/fish", "-l"},
-  font_size = 13.0,
+  font_size = 15.0,
   harfbuzz_features = {"zero"},
   window_background_opacity = 0.9,
   hide_tab_bar_if_only_one_tab = true,
+  colors = {
+    background = "#00141d",
+    foreground = "white"
+  }
 }
