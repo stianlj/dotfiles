3a96a4f7981fe05fff32fd967ad6a97e44975da3
Stian Lund Johansen <stian.lund.johansen@nrk.no>
2021-10-21 18:33:17 +0200

    Add treesitter and onedark

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -1,3 +1,22 @@
 return require('packer').startup(function()
 	use 'wbthomason/packer.nvim'
+	use {
+		'navarasu/onedark.nvim',
+		config = function()
+			vim.g.onedark_style = 'deep'
+			vim.g.onedark_transparent_background = true
+			require('onedark').setup()
+		end
+	}
+	use {
+		'nvim-treesitter/nvim-treesitter',
+		run = ':TSUpdate',
+		config = function()
+			require('nvim-treesitter').setup {
+				highlight = {
+					enable = true
+				}
+			}
+		end
+	}
 end)
