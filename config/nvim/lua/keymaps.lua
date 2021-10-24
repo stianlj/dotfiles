36c84bda4ffe8525613d6d5e56eef8c143310282
Stian Lund Johansen <stian.lund.johansen@nrk.no>
2021-10-24 23:06:15 +0200

    Add convenient keymaps

--- config/nvim/lua/keymaps.lua
+++ config/nvim/lua/keymaps.lua
@@ -2,6 +2,17 @@ local map = vim.api.nvim_set_keymap
 local default_opts = {noremap = true, silent = true}
 local cmd = vim.cmd
 
+-- don't use arrow keys
+map('', '<up>', '<nop>', {noremap = true})
+map('', '<down>', '<nop>', {noremap = true})
+map('', '<left>', '<nop>', {noremap = true})
+map('', '<right>', '<nop>', {noremap = true})
+
+-- fast saving with <leader> and s
+map('n', '<leader>s', ':w<CR>', default_opts)
+map('i', '<leader>s', '<C-c>:w<CR>', default_opts)
+
+-- move around splits using Ctrl + {h,j,k,l}
 map('n', '<C-h>', '<C-w>h', default_opts)
 map('n', '<C-j>', '<C-w>j', default_opts)
 map('n', '<C-k>', '<C-w>k', default_opts)
