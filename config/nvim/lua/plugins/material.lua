7d074e100aeb6e9b8a61c44651b2d6dd6028c468
Stian Lund Johansen <stian.lund.johansen@nrk.no>
2021-10-24 19:56:24 +0200

    Set material theme options in setup

--- config/nvim/lua/plugins/material.lua
+++ config/nvim/lua/plugins/material.lua
@@ -1,9 +1,19 @@
 local g = vim.g
 local cmd = vim.cmd
 
-g.material_disable_background = true
 g.material_style = "deep ocean"
 
-require('material').setup()
+require('material').setup({
+  italics = {
+		comments = true,
+		keywords = true,
+		functions = false,
+		strings = false,
+		variables = false
+	},
+  disable = {
+    background = true,
+  },
+})
 
 cmd[[colorscheme material]]
