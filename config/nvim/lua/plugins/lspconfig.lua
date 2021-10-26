9483e8e320e9d548f17ca64daabaf172e43d7cf4
Stian Lund Johansen <stian.lund.johansen@nrk.no>
2021-10-26 20:14:15 +0200

    Update lspconfig

--- config/nvim/lua/plugins/lspconfig.lua
+++ config/nvim/lua/plugins/lspconfig.lua
@@ -7,11 +7,83 @@ lsp_installer.on_server_ready(function(server)
 end)
 
 local lspconfig = require('lspconfig')
-local servers = { 'tsserver', 'eslint', 'sumneko_lua' }
-for _, server in ipairs(servers) do
-  lspconfig[server].setup({
-    flags = {
-      debounce_text_changes = 150
-    }
-  })
-end
+
+local default_opts = {
+  flags = {
+    debounce_text_changes = 150
+  }
+}
+lspconfig.tsserver.setup(default_opts)
+lspconfig.eslint.setup(default_opts)
+lspconfig.sumneko_lua.setup(default_opts)
+lspconfig.intelephense.setup({
+  settings = {
+    intelephense = {
+      stubs = {
+        "bcmath",
+        "bz2",
+        "calendar",
+        "Core",
+        "curl",
+        "date",
+        "dba",
+        "dom",
+        "enchant",
+        "fileinfo",
+        "filter",
+        "ftp",
+        "gd",
+        "gettext",
+        "hash",
+        "iconv",
+        "imap",
+        "intl",
+        "json",
+        "ldap",
+        "libxml",
+        "mbstring",
+        "mcrypt",
+        "mysql",
+        "mysqli",
+        "password",
+        "pcntl",
+        "pcre",
+        "PDO",
+        "pdo_mysql",
+        "Phar",
+        "readline",
+        "recode",
+        "Reflection",
+        "regex",
+        "session",
+        "SimpleXML",
+        "soap",
+        "sockets",
+        "sodium",
+        "SPL",
+        "standard",
+        "superglobals",
+        "sysvsem",
+        "sysvshm",
+        "tokenizer",
+        "xml",
+        "xdebug",
+        "xmlreader",
+        "xmlwriter",
+        "yaml",
+        "zip",
+        "zlib",
+        "wordpress",
+        "woocommerce",
+        "acf-pro",
+        "wordpress-globals",
+        "wp-cli",
+        "genesis",
+        "polylang"
+      },
+      files = {
+        maxSize = 5000000
+      };
+    };
+  },
+})
