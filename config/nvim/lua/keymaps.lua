89705d494b4625b5286467b6c6cfca6a722a6513
Stian Lund Johansen <stian.lund.johansen@nrk.no>
2021-10-27 17:57:23 +0200

    Use which key to register leader maps

--- config/nvim/lua/keymaps.lua
+++ config/nvim/lua/keymaps.lua
@@ -1,6 +1,32 @@
 local map = vim.api.nvim_set_keymap
 local default_opts = {noremap = true, silent = true}
-local cmd = vim.cmd
+local whichKey = require('which-key')
+
+whichKey.register({
+  ['<Space>'] = { '<cmd>Telescope find_files<CR>', 'Find file' },
+  ['.'] = { '<cmd>Telescope file_browser<CR>', 'Open filebrowser' },
+  ['/'] = { '<cmd>Telescope live_grep<CR>', 'Search in files' },
+  s = { ':w<CR>', 'Save buffer' },
+  b = {
+    name = 'Buffers',
+    b = { '<cmd>Telescope buffers<CR>', 'Switch buffers' },
+  },
+  c = {
+    name = 'Code',
+    r = { '<cmd>lua vim.lsp.buf.rename()<CR>', 'LSP rename' },
+    x = { '<cmd>TroubleToggle<CR>', 'LSP project diagnostics' }
+  },
+  g = {
+    name = 'Git',
+    -- Since NeoGit doesn't seem to cut it just yet
+    g = { '<cmd>term emacsclient -nw -e "(magit-status)"<CR>', 'Magit status' },
+    s = { '<cmd>Telescope git_status<CR>', 'Git status' },
+  },
+  o = {
+    name = 'Open',
+    p = { '<cmd>NvimTreeToggle<CR>', 'Project sidebar' },
+  },
+}, { prefix = '<leader>' })
 
 -- don't use arrow keys
 map('', '<up>', '<nop>', {noremap = true})
@@ -8,33 +34,17 @@ map('', '<down>', '<nop>', {noremap = true})
 map('', '<left>', '<nop>', {noremap = true})
 map('', '<right>', '<nop>', {noremap = true})
 
--- fast saving with <leader> and s
-map('n', '<leader>s', ':w<CR>', default_opts)
-
 -- move around splits using Ctrl + {h,j,k,l}
 map('n', '<C-h>', '<C-w>h', default_opts)
 map('n', '<C-j>', '<C-w>j', default_opts)
 map('n', '<C-k>', '<C-w>k', default_opts)
 map('n', '<C-l>', '<C-w>l', default_opts)
 
-map('n', '<leader><Space>', '<cmd>Telescope find_files<CR>', default_opts)
-map('n', '<leader>.', '<cmd>Telescope file_browser<CR>', default_opts)
-map('n', '<leader>bb', '<cmd>Telescope buffers<CR>', default_opts)
-map('n', '<leader>gs', '<cmd>Telescope git_status<CR>', default_opts)
-map('n', '<leader>/', '<cmd>Telescope live_grep<CR>', default_opts)
 -- A poor mans meta-x in Vim
 map('n', '<M-x>', '<cmd>Telescope commands<CR>', default_opts)
 
-map('n', '<leader>op', '<cmd>NvimTreeToggle<CR>', default_opts)
-
-map('n', '<leader>xx', '<cmd>TroubleToggle<CR>', default_opts)
 map('n', '<leader>xw', '<cmd>TroubleToggle lsp_workspace_diagnostics<CR>', default_opts)
 map('n', '<leader>xd', '<cmd>TroubleToggle lsp_document_diagnostics<CR>', default_opts)
 map('n', '<leader>xq', '<cmd>TroubleToggle quickfix<CR>', default_opts)
 map('n', '<leader>xl', '<cmd>TroubleToggle loclist<CR>', default_opts)
 map('n', 'gR', '<cmd>TroubleToggle lsp_references<CR>', default_opts)
-
-map('n', '<leader>cr', '<cmd>lua vim.lsp.buf.rename()<CR>', default_opts)
-
--- Since NeoGit doesn't seem to cut it just yet
-map('n', '<leader>gg', '<cmd>term emacsclient -nw -e "(magit-status)"<CR>', {noremap = true})
