c15a19799e1f090151a655dd82734d19c1c97d32
Stian Lund Johansen <stian.lund.johansen@nrk.no>
2021-10-28 15:21:16 +0200

    Add prettier(d) as formatter

--- config/nvim/lua/plugins/formatter.lua
+++ config/nvim/lua/plugins/formatter.lua
@@ -2,7 +2,7 @@ vim.api.nvim_exec(
   [[
 augroup FormatAutogroup
   autocmd!
-  autocmd BufWritePost *.lua FormatWrite
+  autocmd BufWritePost *.lua,*.js,*.mjs FormatWrite
 augroup END
 ]],
   true
@@ -15,6 +15,16 @@ require("formatter").setup({
         return {
           exe = "stylua",
           args = { "--search-parent-directories -" },
+          stdin = true,
+        }
+      end,
+    },
+    javascript = {
+      logging = false,
+      function()
+        return {
+          exe = "prettierd",
+          args = { vim.api.nvim_buf_get_name(0) },
           stdin = true,
         }
       end,
