843ed4acb291ace116c8b7778f824842adf9f278
Stian Lund Johansen <stian@lundjohansen.no>
2021-11-13 11:54:26 +0100

    Move lualine config

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -43,13 +43,7 @@ return packer.startup(function(use)
     "nvim-lualine/lualine.nvim",
     requires = { "kyazdani42/nvim-web-devicons", opt = true },
     config = function()
-      require("lualine").setup({
-        options = {
-          theme = "tokyonight",
-          section_separators = { left = "", right = "" },
-          component_separators = {},
-        },
-      })
+      require("plugins/lualine-config")
     end,
   })
   use({
