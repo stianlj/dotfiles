d1e65c4b494a88a62aa2aea9bd895edf6aa9b404
Stian Lund Johansen <stian.lund.johansen@nrk.no>
2021-12-02 12:41:25 +0100

    Add formatters and refactor

--- config/nvim/lua/plugins/formatter-config.lua
+++ config/nvim/lua/plugins/formatter-config.lua
@@ -2,39 +2,34 @@ vim.api.nvim_exec(
   [[
 augroup FormatAutogroup
   autocmd!
-  autocmd BufWritePost *.lua,*.js,*.mjs,*.yaml,*.yml FormatWrite
+  autocmd BufWritePost *.lua,*.js,*.mjs,*.ts,*.tsx,*.yaml,*.yml FormatWrite
 augroup END
 ]],
   true
 )
 
+local prettierdConfig = {
+  logging = false,
+  function()
+    return {
+      exe = "prettierd",
+      args = { vim.api.nvim_buf_get_name(0) },
+      stdin = true,
+    }
+  end,
+}
+
 require("formatter").setup({
   filetype = {
+    javascript = prettierdConfig,
+    typescript = prettierdConfig,
+    typescriptreact = prettierdConfig,
+    yaml = prettierdConfig,
     lua = {
       function()
         return {
           exe = "stylua",
           args = { "--search-parent-directories -" },
-          stdin = true,
-        }
-      end,
-    },
-    javascript = {
-      logging = false,
-      function()
-        return {
-          exe = "prettierd",
-          args = { vim.api.nvim_buf_get_name(0) },
-          stdin = true,
-        }
-      end,
-    },
-    yaml = {
-      logging = false,
-      function()
-        return {
-          exe = "prettierd",
-          args = { vim.api.nvim_buf_get_name(0) },
           stdin = true,
         }
       end,
