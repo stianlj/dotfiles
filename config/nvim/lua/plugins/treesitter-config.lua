c621e2dfe5d2b842a01b524e3d7d071f176444a8
Stian Lund Johansen <stian@lundjohansen.no>
2021-12-13 16:56:24 +0100

    feat: Remove treesitter swap, since I dont use it

--- config/nvim/lua/plugins/treesitter-config.lua
+++ config/nvim/lua/plugins/treesitter-config.lua
@@ -27,15 +27,6 @@ require("nvim-treesitter.configs").setup({
         ["ic"] = "@class.inner",
       },
     },
-    swap = {
-      enable = true,
-      swap_next = {
-        ["<leader>a"] = "@parameter.inner",
-      },
-      swap_previous = {
-        ["<leader>A"] = "@parameter.inner",
-      },
-    },
   },
   playground = {
     enable = true,
