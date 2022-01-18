5e0dcf5c404abd957bbdf4ecd202ff50b3461bd2
Stian Lund Johansen <stian@lundjohansen.no>
2022-01-18 22:15:28 +0100

    chore: fix breaking change

--- config/nvim/lua/plugins/neogit-config.lua
+++ config/nvim/lua/plugins/neogit-config.lua
@@ -4,6 +4,7 @@ neogit.config.use_magit_keybindings()
 
 neogit.setup({
   disable_insert_on_commit = false,
+  use_magit_keybindings = true,
   signs = {
     section = { "", "" },
     item = { "", "" },
