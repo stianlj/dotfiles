c35b5ca098a2a1c2a9d56facc58716a611cf92ba
Stian Lund Johansen <stian@lundjohansen.no>
2022-02-28 23:38:23 +0100

    feat: use xkb to set compose key

--- bin/start_river.sh
+++ bin/start_river.sh
@@ -3,6 +3,9 @@
 export MOZ_ENABLE_WAYLAND=1
 export XDG_SESSION_TYPE=wayland
 export XDG_CURRENT_DESKTOP=river
+export GTK_USE_PORTAL=0
+export GDK_BACKEND=wayland
+export XKB_DEFAULT_OPTIONS=compose:ralt
 
 timestamp=$(date +%F-%R)
-exec dbus-run-session river -log-level debug >/tmp/river-${timestamp}.log 2>&1
+exec dbus-run-session river -log-level debug >/tmp/river-"$timestamp".log 2>&1
