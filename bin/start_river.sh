53cbf0690685c4f3c0087d721b53aaedb28443eb
Stian Lund Johansen <stian.lund.johansen@nrk.no>
2022-03-17 22:56:09 +0100

    feat(river): set QT_QPA_PLATFORM

--- bin/start_river.sh
+++ bin/start_river.sh
@@ -5,6 +5,7 @@ export XDG_SESSION_TYPE=wayland
 export XDG_CURRENT_DESKTOP=river
 export GTK_USE_PORTAL=0
 export GDK_BACKEND=wayland
+export QT_QPA_PLATFORM=wayland
 export XKB_DEFAULT_OPTIONS=compose:ralt
 
 timestamp=$(date +%F-%R)
