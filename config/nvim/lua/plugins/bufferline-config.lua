a3c14ecaa34ea243bfc533332e82c305d0ac09c5
Stian Lund Johansen <stian@lundjohansen.no>
2022-04-08 18:22:21 +0200

    feat(nvim): add ordinal numbers to bufferline

--- config/nvim/lua/plugins/bufferline-config.lua
+++ config/nvim/lua/plugins/bufferline-config.lua
@@ -3,5 +3,8 @@ require("bufferline").setup({
     diagnostics = "nvim_lsp",
     offsets = { { filetype = "NvimTree", text = "File Explorer", text_align = "left" } },
     separator_style = "slant",
+    numbers = function(opts)
+      return string.format("%s·%s", opts.ordinal, opts.raise(opts.id))
+    end,
   },
 })
