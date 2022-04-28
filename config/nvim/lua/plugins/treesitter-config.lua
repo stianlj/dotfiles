d3f0165898ee65bb6aa09c4e65e1327d66c4113a
Stian Lund Johansen <stian@lundjohansen.no>
2022-04-28 08:36:59 +0200

    feat(nvim): turn off vim regex syntax hightlight

--- config/nvim/lua/plugins/treesitter-config.lua
+++ config/nvim/lua/plugins/treesitter-config.lua
@@ -3,7 +3,7 @@ require("nvim-treesitter.configs").setup({
   highlight = {
     enable = true,
     use_languagetree = true,
-    additional_vim_regex_highlighting = true,
+    additional_vim_regex_highlighting = false,
   },
   rainbow = {
     enable = true,
