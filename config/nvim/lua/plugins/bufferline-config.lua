b03ddf7d6dad0ed538c5e594e3685e210d2c5302
Stian Lund Johansen <stian@lundjohansen.no>
2022-05-26 14:13:29 +0200

    feat(nvim): remove slant separator bufferline

--- config/nvim/lua/plugins/bufferline-config.lua
+++ config/nvim/lua/plugins/bufferline-config.lua
@@ -2,7 +2,6 @@ require("bufferline").setup({
   options = {
     diagnostics = "nvim_lsp",
     offsets = { { filetype = "NvimTree", text = "File Explorer", text_align = "left" } },
-    separator_style = "slant",
     numbers = function(opts)
       return string.format("%s·%s", opts.ordinal, opts.raise(opts.id))
     end,
