7c873ac95e5196fe1ebb827bc5bf87eb3e659286
Stian Lund Johansen <stian@lundjohansen.no>
2022-10-04 13:21:13 +0200

    feat(nvim): make null-ls 0.8 compatible

--- config/nvim/lua/plugins/null-ls-config.lua
+++ config/nvim/lua/plugins/null-ls-config.lua
@@ -2,7 +2,7 @@ local null_ls = require("null-ls")
 
 null_ls.setup({
   on_attach = function(client)
-    if client.resolved_capabilities.document_formatting then
+    if client.server_capabilities.document_formatting then
       vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()")
     end
   end,
