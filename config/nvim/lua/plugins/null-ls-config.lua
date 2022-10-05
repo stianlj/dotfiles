f2c738ee100341f4fcebb6827acdb082b744a8de
Stian Lund Johansen <stian@lundjohansen.no>
2022-10-05 14:04:37 +0200

    fix(nvim): fix null-ls for 0.8

--- config/nvim/lua/plugins/null-ls-config.lua
+++ config/nvim/lua/plugins/null-ls-config.lua
@@ -1,9 +1,22 @@
 local null_ls = require("null-ls")
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 null_ls.setup({
-  on_attach = function(client)
-    if client.server_capabilities.document_formatting then
-      vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()")
+  on_attach = function(client, bufnr)
+    if client.supports_method("textDocument/formatting") then
+      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = augroup,
+        buffer = bufnr,
+        callback = function()
+          vim.lsp.buf.format({
+            bufnr = bufnr,
+            filter = function(c)
+              return c.name == "null-ls"
+            end
+          })
+        end,
+      })
     end
   end,
   sources = {
