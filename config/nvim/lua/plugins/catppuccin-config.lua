4d1daab34d32cd06db097a68343775e2bc4aa0fd
Stian Lund Johansen <stian@lundjohansen.no>
2022-10-10 08:48:55 +0200

    feat(nvim): dim inactive windows

--- config/nvim/lua/plugins/catppuccin-config.lua
+++ config/nvim/lua/plugins/catppuccin-config.lua
@@ -2,6 +2,9 @@ vim.g.catppuccin_flavour = "mocha"
 local colors = require("catppuccin.palettes").get_palette()
 
 require("catppuccin").setup({
+  dim_inactive = {
+    enabled = true,
+  },
   custom_highlights = {
     IndentBlanklineContextChar = { fg = colors.yellow },
   },
