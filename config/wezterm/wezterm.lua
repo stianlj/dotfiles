abf84658b60e404f69beba1ebfe094bde1d16a4b
Stian Lund Johansen <stian@lundjohansen.no>
2023-01-31 22:41:40 +0100

    feat(wezterm): add bindings to tab. add script glyphs

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -1,7 +1,14 @@
 local wezterm = require("wezterm")
+local action = wezterm.action
+
 -- TODO: https://github.com/prabirshrestha/dotfiles/blob/main/.config/wezterm/wezterm.lua
 return {
-  font = wezterm.font("MonoLisa"),
+  font = wezterm.font({
+    -- INFO: Read https://wezfurlong.org/wezterm/config/font-shaping.html for more advanced font shaping
+    -- And https://www.monolisa.dev/faq#how-to-enable-stylistic-sets-for-the-kitty-terminal
+    family = "MonoLisa",
+    harfbuzz_features = { "ss02=1" },
+  }),
   default_prog = { "/usr/bin/fish", "-l" },
   font_size = 12.0,
   use_fancy_tab_bar = false,
@@ -16,8 +23,18 @@ return {
     bottom = 0,
   },
   keys = {
-    { key = "c", mods = "ALT", action = wezterm.action.CopyTo("ClipboardAndPrimarySelection") },
-    { key = "v", mods = "ALT", action = wezterm.action.PasteFrom("Clipboard") },
-    { key = "v", mods = "ALT", action = wezterm.action.PasteFrom("PrimarySelection") },
+    { key = "c", mods = "ALT", action = action.CopyTo("ClipboardAndPrimarySelection") },
+    { key = "v", mods = "ALT", action = action.PasteFrom("Clipboard") },
+    { key = "v", mods = "ALT", action = action.PasteFrom("PrimarySelection") },
+    { key = "t", mods = "SHIFT|ALT", action = action.SpawnTab("CurrentPaneDomain") },
+    { key = "1", mods = "SHIFT|ALT", action = action.ActivateTab(0) },
+    { key = "2", mods = "SHIFT|ALT", action = action.ActivateTab(1) },
+    { key = "3", mods = "SHIFT|ALT", action = action.ActivateTab(2) },
+    { key = "4", mods = "SHIFT|ALT", action = action.ActivateTab(3) },
+    { key = "5", mods = "SHIFT|ALT", action = action.ActivateTab(4) },
+    { key = "6", mods = "SHIFT|ALT", action = action.ActivateTab(5) },
+    { key = "7", mods = "SHIFT|ALT", action = action.ActivateTab(6) },
+    { key = "8", mods = "SHIFT|ALT", action = action.ActivateTab(7) },
+    { key = "9", mods = "SHIFT|ALT", action = action.ActivateTab(8) },
   },
 }
