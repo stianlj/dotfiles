3471d7c3c4ab0f885abd93c36acb2aea646f6fdc
Stian Lund Johansen <stian@lundjohansen.no>
2023-02-04 15:15:30 +0100

    feat(nvim): add a toggable terminal

--- config/nvim/lua/plugins/bufterm.lua
+++ config/nvim/lua/plugins/bufterm.lua
@@ -1,4 +1,20 @@
 return {
   "boltlessengineer/bufterm.nvim",
-  config = true,
+  config = function()
+    require("bufterm").setup()
+    local Terminal = require("bufterm.terminal").Terminal
+    local ui = require("bufterm.ui")
+    local term = Terminal:new({
+      cmd = "/usr/bin/fish",
+      buflisted = false,
+      termlisted = false,
+    })
+
+    vim.keymap.set({ "n", "t" }, "<C-t>", function()
+      term:spawn()
+      ui.toggle_float(term.bufnr)
+    end, {
+      desc = "Toggle floating window with terminal buffers",
+    })
+  end,
 }
