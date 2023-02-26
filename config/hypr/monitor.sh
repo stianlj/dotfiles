f979025d2e91fcb47dc0757c4b75b16d56f178f1
Stian Lund Johansen <stian@lundjohansen.no>
2023-02-26 17:26:25 +0100

    feat(hypr): consolidate config between laptop and desktop

--- config/hypr/monitor.sh
+++ config/hypr/monitor.sh
@@ -12,19 +12,19 @@ if [ "$COMPUTER_IDENTIFIER" = "main-desktop" ]; then
   OUTPUT_MAIN="DP-1"
   OUTPUT_UPPER="HDMI-A-1"
   CONFIG=(
+    "keyword wsbind 1,$OUTPUT_MAIN"
+    "keyword wsbind 2,$OUTPUT_MAIN"
+    "keyword wsbind 3,$OUTPUT_MAIN"
+    "keyword wsbind 4,$OUTPUT_MAIN"
+    "keyword wsbind 5,$OUTPUT_MAIN"
+    "keyword wsbind 6,$OUTPUT_MAIN"
+    "keyword wsbind 7,$OUTPUT_UPPER"
+    "keyword wsbind 8,$OUTPUT_UPPER"
+    "keyword wsbind 9,$OUTPUT_UPPER"
+    "keyword wsbind 10,$OUTPUT_MAIN"
     "keyword monitor $OUTPUT_MAIN,5120x1440,499x1545,1.4"
     "keyword monitor $OUTPUT_MAIN,addreserved,0,0,400,447"
     "keyword monitor $OUTPUT_UPPER,3840x2160,1018x0,1.4"
-    # "keyword wsbind 1,$OUTPUT_MAIN"
-    # "keyword wsbind 2,$OUTPUT_MAIN"
-    # "keyword wsbind 3,$OUTPUT_MAIN"
-    # "keyword wsbind 4,$OUTPUT_MAIN"
-    # "keyword wsbind 5,$OUTPUT_MAIN"
-    # "keyword wsbind 6,$OUTPUT_MAIN"
-    # "keyword wsbind 7,$OUTPUT_UPPER"
-    # "keyword wsbind 8,$OUTPUT_UPPER"
-    # "keyword wsbind 9,$OUTPUT_UPPER"
-    # "keyword wsbind 10,$OUTPUT_MAIN"
   )
   separator=" ; "
   string="$(printf "${separator}%s" "${CONFIG[@]}")"
@@ -46,6 +46,7 @@ elif [ "$COMPUTER_IDENTIFIER" = "work-laptop" ]; then
       "keyword monitor $OUTPUT_LEFT,3840x2400,auto,3"
       "keyword monitor $OUTPUT_CENTER,5120x1440,1280x0,1.4"
       "keyword monitor $OUTPUT_CENTER,addreserved,10,10,400,447"
+      "keyword dwindle:no_gaps_when_only yes"
     )
     separator=" ; "
     string="$(printf "${separator}%s" "${CONFIG[@]}")"
@@ -57,20 +58,21 @@ elif [ "$COMPUTER_IDENTIFIER" = "work-laptop" ]; then
     OUTPUT_CENTER="DP-2"
     OUTPUT_RIGHT="DP-5"
     CONFIG=(
+      "keyword wsbind 1,$OUTPUT_LEFT"
+      "keyword wsbind 2,$OUTPUT_LEFT"
+      "keyword wsbind 3,$OUTPUT_CENTER"
+      "keyword wsbind 4,$OUTPUT_CENTER"
+      "keyword wsbind 5,$OUTPUT_CENTER"
+      "keyword wsbind 6,$OUTPUT_CENTER"
+      "keyword wsbind 7,$OUTPUT_RIGHT"
+      "keyword wsbind 8,$OUTPUT_RIGHT"
+      "keyword wsbind 9,$OUTPUT_RIGHT"
+      "keyword wsbind 10,$OUTPUT_RIGHT"
       "keyword monitor $OUTPUT_CENTER,addreserved,0,0,47,0"
       "keyword monitor $OUTPUT_LEFT,3840x2400,auto,3"
       "keyword monitor $OUTPUT_CENTER,3840x2160,1280x0,2"
       "keyword monitor $OUTPUT_RIGHT,2560x1440,3200x0,1.4"
-      # "keyword wsbind 1,$OUTPUT_LEFT"
-      # "keyword wsbind 2,$OUTPUT_LEFT"
-      # "keyword wsbind 3,$OUTPUT_CENTER"
-      # "keyword wsbind 4,$OUTPUT_CENTER"
-      # "keyword wsbind 5,$OUTPUT_CENTER"
-      # "keyword wsbind 6,$OUTPUT_CENTER"
-      # "keyword wsbind 7,$OUTPUT_RIGHT"
-      # "keyword wsbind 8,$OUTPUT_RIGHT"
-      # "keyword wsbind 9,$OUTPUT_RIGHT"
-      # "keyword wsbind 10,$OUTPUT_RIGHT"
+      "keyword dwindle:no_gaps_when_only yes"
     )
 
     # run_hyprctl "$CONFIG"
@@ -81,7 +83,7 @@ elif [ "$COMPUTER_IDENTIFIER" = "work-laptop" ]; then
     eww open "bar-triple"
   else
     # monitor=DP-1,1920x1080,0x0,1,mirror,eDP-1
-    hyprctl --batch "keyword monitor eDP-1,addreserved,0,0,47,0 ; keyword monitor eDP-1,3840x2400,auto,2"
+    hyprctl --batch "keyword monitor eDP-1,addreserved,0,0,47,0 ; keyword monitor eDP-1,3840x2400,auto,2 ; keyword dwindle:no_gaps_when_only yes"
     eww open bar
   fi
 fi
