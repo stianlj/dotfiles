d286aafb2eb14afaf8b55eedd72980249e959028
Stian Lund Johansen <stian@lundjohansen.no>
2023-03-19 22:04:44 +0100

    feat(emacs): give even more breathing room

--- config/emacs/init.el
+++ config/emacs/init.el
@@ -29,7 +29,7 @@
 (scroll-bar-mode -1)        ; Disable visible scrollbar
 (tool-bar-mode -1)          ; Disable the toolbar
 (tooltip-mode -1)           ; Disable tooltips
-(set-fringe-mode 10)        ; Give some breathing room
+(set-fringe-mode 15)        ; Give some breathing room
 
 (menu-bar-mode -1)            ; Disable the menu bar
 
