c71d633ca16e07bf9c855e90e1b8c2ed1d87d968
Stian Lund Johansen <stian@lundjohansen.no>
2023-05-03 23:40:56 +0200

    feat(nvim): make statuscol a bit prettier

--- config/nvim/lua/plugins/statuscol.lua
+++ config/nvim/lua/plugins/statuscol.lua
@@ -1,3 +1,4 @@
+-- TODO: Toggle breakpoint kind of look kind of out of place. It also moves the gitsigns
 return {
   "luukvbaal/statuscol.nvim",
   config = function()
@@ -7,12 +8,18 @@ return {
       segments = {
         { text = { builtin.foldfunc }, click = "v:lua.ScFa" },
         {
-          sign = { name = { "Diagnostic" }, maxwidth = 2, auto = true },
+          sign = { name = { "Diagnostic" }, maxwidth = 1 },
           click = "v:lua.ScSa",
+        },
+        {
+          sign = {
+            name = { "todo*" },
+            maxwidth = 1,
+          },
         },
         { text = { builtin.lnumfunc }, click = "v:lua.ScLa" },
         {
-          sign = { name = { ".*" }, maxwidth = 2, colwidth = 1, auto = true, wrap = true },
+          sign = { name = { ".*" }, maxwidth = 2, colwidth = 1, wrap = true },
           click = "v:lua.ScSa",
         },
       },
