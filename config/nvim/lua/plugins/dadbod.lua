98f8813515a4ca615e1710d673b97afb1b5f1b9b
Stian Lund Johansen <stian@lundjohansen.no>
2023-09-10 02:20:24 +0200

    feat: use nerdfonts for dadbod ui

--- config/nvim/lua/plugins/dadbod.lua
+++ config/nvim/lua/plugins/dadbod.lua
@@ -1,5 +1,10 @@
 return {
   "tpope/vim-dadbod",
-  "kristijanhusak/vim-dadbod-ui",
   "kristijanhusak/vim-dadbod-completion",
+  {
+    "kristijanhusak/vim-dadbod-ui",
+    init = function()
+      vim.g.db_ui_use_nerd_fonts = 1
+    end,
+  },
 }
