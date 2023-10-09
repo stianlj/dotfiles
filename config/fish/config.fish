edd98421d8d5a55b53e1c00171d9858418c5765c
Stian Lund Johansen <stian@lundjohansen.no>
2023-10-09 09:34:55 +0200

    feat(system): add volta path

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -8,6 +8,7 @@ set -gx VOLTA_HOME $HOME/.volta
 
 test -d "$HOME/.local/bin"; and set PATH "$HOME/.local/bin" $PATH
 test -d "$HOME/.npm-global/bin"; and set PATH "$HOME/.npm-global/bin" $PATH
+test -d "$HOME/.volta/bin"; and set PATH "$HOME/.volta/bin" $PATH
 test -d "$VOLTA_HOME/bin"; and set PATH "$VOLTA_HOME/bin" $PATH
 
 bind yy fish_clipboard_copy
