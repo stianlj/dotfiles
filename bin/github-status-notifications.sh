4a1332e8e6780e5837f4e84eacb213ce8d221d93
Stian Lund Johansen <stian@lundjohansen.no>
2023-11-02 22:07:21 +0100

    feat(waybar): tooltip in github notifications

--- bin/github-status-notifications.sh
+++ bin/github-status-notifications.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 
-github_notifications_json=$(gh api notifications | jq '. | length')
+github_notifications_json=$(gh api notifications)
 github_notifications_number=$(echo "$github_notifications_json" | jq '. | length')
-# github_notifications_summary=$(echo "$github_notifications_json" | jq -r '.[] | (.repository.name + ": " + .title)')
+github_notifications_summary=$(echo "$github_notifications_json" | jq -r '.[] | (.repository.name + ": " + .subject.title)')
 
 if ! [[ $github_notifications_number == "0" ]]; then
-  # echo "{\"text\": \"${github_notifications_number}\", \"class\": \"has-pr\", \"tooltip\": \"${github_notifications_summary//$'\n'/\\n}\"}"
-  echo "{\"text\": \"${github_notifications_number}\", \"class\": \"has-notifications\"}"
+  echo "{\"text\": \"${github_notifications_number}\", \"class\": \"has-notifications\", \"tooltip\": \"${github_notifications_summary//$'\n'/\\n}\"}"
 else
   echo "{\"text\": \"${github_notifications_number}\", \"class\": \"\" }"
 fi
