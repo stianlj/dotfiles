c1050d4e64a08d43a461c466cd8539d8bb6384f2
Stian Lund Johansen <stian@lundjohansen.no>
2023-11-06 23:09:32 +0100

    feat(nvim): use persistence.nvim instead of possessesion

--- config/nvim/lua/plugins-legacy/telescope-config.lua
+++ config/nvim/lua/plugins-legacy/telescope-config.lua
@@ -22,5 +22,4 @@ telescope.setup({
 telescope.load_extension("fzf")
 telescope.load_extension("ui-select")
 telescope.load_extension("file_browser")
-telescope.load_extension("possession")
 telescope.load_extension("harpoon")
