e9503d407c1748d19fed2fa8a03ee30758b9e4e8
Stian Lund Johansen <stian@lundjohansen.no>
2024-08-30 00:02:25 +0200

    feat: add parrot.nvim with anthropic

---
+++ config/nvim/lua/plugins/parrot.lua
@@ -0,0 +1,33 @@
+return {
+  "frankroeder/parrot.nvim",
+  dependencies = { "ibhagwan/fzf-lua", "nvim-lua/plenary.nvim", "rcarriga/nvim-notify" },
+  config = function()
+    require("parrot").setup({
+      providers = {
+        anthropic = {
+          api_key = os.getenv("ANTHROPIC_API_KEY"),
+        },
+        -- gemini = {
+        --   api_key = os.getenv("GEMINI_API_KEY"),
+        -- },
+        -- groq = {
+        --   api_key = os.getenv("GROQ_API_KEY"),
+        -- },
+        -- mistral = {
+        --   api_key = os.getenv("MISTRAL_API_KEY"),
+        -- },
+        -- pplx = {
+        --   api_key = os.getenv("PERPLEXITY_API_KEY"),
+        -- },
+        -- -- provide an empty list to make provider available (no API key required)
+        -- ollama = {},
+        -- openai = {
+        --   api_key = os.getenv("OPENAI_API_KEY"),
+        -- },
+        -- github = {
+        --   api_key = os.getenv("GITHUB_TOKEN"),
+        -- },
+      },
+    })
+  end,
+}
