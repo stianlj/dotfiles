747f4f06603009592d82c7305d501862c131d052
Stian Lund Johansen <stian@lundjohansen.no>
2025-05-24 13:10:36 +0200

    feat(parrot): remove unused ollama provider from plugin config

--- config/nvim/lua/plugins/parrot.lua
+++ config/nvim/lua/plugins/parrot.lua
@@ -7,7 +7,6 @@ return {
         anthropic = {
           api_key = os.getenv("ANTHROPIC_API_KEY"),
         },
-        ollama = {},
         openai = {
           api_key = os.getenv("OPENAI_API_KEY"),
         },
