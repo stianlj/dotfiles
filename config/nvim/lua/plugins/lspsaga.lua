8e5ecd2b86f2e705ba34490f79fa10e433a79ec2
Stian Lund Johansen <stian@lundjohansen.no>
2025-06-17 23:21:04 +0200

    refactor(lspsaga): update plugin source and configuration style
    - Change plugin source from "glepnir/lspsaga.nvim" to "nvimdev/lspsaga.nvim"
    - Replace config function with opts table for plugin setup

--- config/nvim/lua/plugins/lspsaga.lua
+++ config/nvim/lua/plugins/lspsaga.lua
@@ -1,18 +1,16 @@
 return {
-  "glepnir/lspsaga.nvim",
+  "nvimdev/lspsaga.nvim",
   event = "BufRead",
-  config = function()
-    require("lspsaga").setup({
-      lightbulb = {
-        enable = false,
-      },
-      code_action = {
-        num_shortcut = false,
-      },
-      symbol_in_winbar = {
-        enable = false,
-      },
-    })
-  end,
+  opts = {
+    lightbulb = {
+      enable = false,
+    },
+    code_action = {
+      num_shortcut = false,
+    },
+    symbol_in_winbar = {
+      enable = false,
+    },
+  },
   dependencies = { { "nvim-tree/nvim-web-devicons" } },
 }
