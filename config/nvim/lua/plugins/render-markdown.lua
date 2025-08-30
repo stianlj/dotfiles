5832b6d4abd1a6507034a7182f994536aeba0022
Stian Lund Johansen <stian@lundjohansen.no>
2025-08-30 23:46:52 +0200

    feat(nvim): add codecompanion filetype support to render-markdown.nvim plugin

--- config/nvim/lua/plugins/render-markdown.lua
+++ config/nvim/lua/plugins/render-markdown.lua
@@ -1,5 +1,6 @@
 return {
   "MeanderingProgrammer/render-markdown.nvim",
   opts = {},
+  ft = { "markdown", "codecompanion" },
   dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.icons" },
 }
