82fe3f960b56669e443435c1b01a66fcdef8e503
Stian Lund Johansen <stian@lundjohansen.no>
2025-09-21 22:31:12 +0200

    feat(nvim): add custom checkbox states for render-markdown.nvim and update obsidian.nvim UI/keys config

--- config/nvim/lua/plugins/obsidian.lua
+++ config/nvim/lua/plugins/obsidian.lua
@@ -10,6 +10,7 @@ return {
   ---@type obsidian.config.ClientOpts
   opts = {
     legacy_commands = false,
+    ui = { enable = false },
     workspaces = {
       {
         name = "personal",
@@ -26,11 +27,43 @@ return {
       nvim_cmp = false,
       blink = true,
     },
+    -- checkbox = { order = { " ", ">", "x", "-" } },
+    -- callbacks = {
+    --   enter_note = function(_, note)
+    --     vim.keymap.set("n", "<leader>ntd", function()
+    --       require("obsidian.api").set_checkbox("x")
+    --     end, {
+    --       buffer = note.bufnr,
+    --       desc = "Toggle done checkbox",
+    --     })
+    --   end,
+    -- },
   },
   keys = {
-    { "<leader>nn", "<cmd>Obsidian new<cr>",          desc = "Create a new note" },
-    { "<leader>ns", "<cmd>Obsidian search<cr>",       desc = "Search notes" },
+    { "<leader>nn", "<cmd>Obsidian new<cr>", desc = "Create a new note" },
+    { "<leader>ns", "<cmd>Obsidian search<cr>", desc = "Search notes" },
     { "<leader>nf", "<cmd>Obsidian quick_switch<cr>", desc = "Find a note" },
-    { "<leader>nt", "<cmd>Obsidian today<cr>",        desc = "Show todays daily" },
+    { "<leader>nd", "<cmd>Obsidian today<cr>", desc = "Show todays daily" },
+    -- {
+    --   "<leader>ntd",
+    --   function()
+    --     require("obsidian.api").set_checkbox()
+    --   end,
+    --   desc = "Toggle done checkbox",
+    -- },
+    -- {
+    --   "<leader>ntp",
+    --   function()
+    --     require("obsidian.api").set_checkbox(">")
+    --   end,
+    --   desc = "Toggle in progress checkbox",
+    -- },
+    -- {
+    --   "<leader>ntc",
+    --   function()
+    --     require("obsidian.api").set_checkbox("-")
+    --   end,
+    --   desc = "Toggle cancelled checkbox",
+    -- },
   },
 }
--- config/nvim/lua/plugins/render-markdown.lua
+++ config/nvim/lua/plugins/render-markdown.lua
@@ -1,6 +1,14 @@
 return {
   "MeanderingProgrammer/render-markdown.nvim",
-  opts = {},
+  opts = {
+    checkbox = {
+      custom = {
+        todo = {},
+        in_progress = { raw = "[>]", rendered = "󰥔 ", highlight = "RenderMarkdownTodo", scope_highlight = nil },
+        cancelled = { raw = "[-]", rendered = "󰱒 ", highlight = "RenderMarkdownWarn", scope_highlight = nil },
+      },
+    },
+  },
   ft = { "markdown", "codecompanion" },
   dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.icons" },
 }
