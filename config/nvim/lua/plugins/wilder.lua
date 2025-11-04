55e5c0a46fb2eeb5f8dc5e175085fd86f6e07566
Stian Lund Johansen <stian@lundjohansen.no>
2025-11-04 23:52:23 +0100

    feat(nvim): add noice.nvim plugin for enhanced UI; disable wilder.nvim plugin

--- config/nvim/lua/plugins/wilder.lua
+++ config/nvim/lua/plugins/wilder.lua
@@ -2,6 +2,7 @@ return {
   "gelguy/wilder.nvim",
   build = ":UpdateRemotePlugins",
   event = "VeryLazy",
+  enabled = false,
   dependencies = { "romgrk/fzy-lua-native" },
   config = function()
     local wilder = require("wilder")
