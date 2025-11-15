d551d4e4dd9966f75aa63ce55bcdaaa7f03996e8
Stian Lund Johansen <stian@lundjohansen.no>
2025-11-16 00:57:58 +0100

    refactor(fish): update sesh connect abbreviation to use sesh-sessionizer script

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -22,7 +22,7 @@ alias ssh='kitten ssh'
 alias vi='nvim'
 alias cat='bat'
 
-abbr --add --global s 'kitten @ set-spacing padding=0; sesh connect $(sesh list -t -i | gum filter --limit 1 --no-sort --fuzzy --placeholder \'Pick a sesh\' --height 50 --prompt=\'⚡\'); kitten @ set-spacing padding=default'
+abbr --add --global s 'sesh-sessionizer'
 
 starship init fish | source
 zoxide init --cmd cd fish | source
